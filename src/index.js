927db7fe24a5d2f8590b1c124b3f9c7f3a020278
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-15 00:54:15 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import master from './master.png';
+import visa from './visa.png';
+import chips from './chips.png';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+class Header extends React.Component {
+  render() { return ( <h2 className="header" ><strong>CREDIT CARD</strong></h2> );}
+}
+class Chips extends React.Component {
+  render() { return (  <img src={chips} className="chips" alt="chips"/> );}
+}
+class Logos extends React.Component {
+  render() { return ( 
+  <div class="logos">
+    <img src={master} alt="masterCard" height="47" width="75"/>
+    <img src={visa} alt="visaCard" height="50" width="75"/>
+  </div>
+   );}
+}
+class Number extends React.Component {
+  render() { return (  <p className="number" > 7253 3256 7895 1245</p> ); }
+}
+class Expiration extends React.Component {
+  render() { return (  
+  <div class="exp-date-wrapper">
+    <div class="left-label upper-labels">VALID THRU</div>
+    <div class="exp-date">
+        <div class="upper-labels">MONTH/YEAR</div>
+        <div class="date" title="01/17">01/17</div>
+    </div>
+  </div> ); }
+}
+class Row extends React.Component {
+  render() { return (  
+    <div>
+      <div className="row-card">
+          <p className="secret-number"> 5422</p>
+          <Expiration/>
+      </div>
+      <p className="last">CARDHOLDER</p>
+    </div>
+  
+   ); }
+}
+
+
+
+
+
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card"><Header/><Chips/><Number/><div class="data-card"><Row/><Logos/></div></div>
+    );
+  }
+}
+  
+  ReactDOM.render(<Card />, document.getElementById('root')); 
